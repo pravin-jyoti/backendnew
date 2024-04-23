e659646bc7a9addbf9220fb66ea1411debde8ce5
Ganesh-v-e <ganeshkrthck@gmail.com>
2024-04-24 01:03:46 +0530

    new fix for mango db

--- api/key.js
+++ api/key.js
@@ -2,6 +2,6 @@ module.exports = {
     mongoURI: 'mongodb+srv://new_user_123:qwerty123@cluster0.xpfjogw.mongodb.net/',
     options: {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     }
 };
--- index.js
+++ index.js
@@ -12,18 +12,24 @@ const todo = require('./schemas/oldUser');
 const app = express();
 const port = 5500;
 
+const uri = 'mongodb+srv://new_user_123:qwerty123@cluster0.xpfjogw.mongodb.net/'; // Replace 'mydatabase' with your actual database name
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+  // Your code here
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
 
 //DB connection
 const {mongoURI , options} = require('./api/key')
 mongoose.connect(mongoURI, options).then(() => console.log("success")).catch(err => console.log("failure", err))
-
-mongoose.connect(uri, {
-    bufferCommands: false, // Disable buffering
-    bufferTimeoutMS: 10000, // Increase timeout to 10 seconds
-  });
-  
-
-
 //middleware why
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
