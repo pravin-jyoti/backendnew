52cd168a662d0811c16a8497db4a19f79f05195d
Ganesh-v-e <ganeshkrthck@gmail.com>
2024-04-24 01:10:38 +0530

    final

--- api/key.js
+++ api/key.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://new_user_123:qwerty123@cluster0.xpfjogw.mongodb.net/',
+    mongoURI: 'mongodb+srv://new_user_123:qwerty123@cluster0.xpfjogw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
