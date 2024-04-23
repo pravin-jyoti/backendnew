2706c2e072582b87fd0f0abaefb8c407821798d1
pravin-jyoti <167900404+pravin-jyoti@users.noreply.github.com>
2024-04-23 23:16:25 +0530

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://new-user-1:qwerty123@cluster0.cqebwax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
